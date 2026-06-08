870a2859e680d4754da865df1593e9aa34d38b77
Panh <quangdamdepgiai@gmail.com>
2026-06-08 14:24:55 +0700

    Refactor outlier detection logic in outlier_check.py for driver dataset metrics

--- outlier_check.py
+++ outlier_check.py
@@ -1,6 +1,18 @@
 import pandas as pd
 df = pd.read_csv("driver_dataset.csv")
-print(df[df["EAR"] <= 0])
-print(df[df["MAR"] <= 0])
-print(df[df["Pitch"].abs() > 90])
-print(df[df["Yaw"].abs() > 90])
+features = ["EAR", "MAR", "Pitch", "Yaw"]
+
+for col in features:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+
+        IQR = Q3 - Q1
+        lower = Q1 - 1.5 * IQR
+        upper = Q3 + 1.5 * IQR
+
+        outliers = df[(df[col] < lower) | (df[col] > upper)]
+
+        print(col)\
+        
+        print("Outliers:", len(outliers))
+        print(outliers.head())
